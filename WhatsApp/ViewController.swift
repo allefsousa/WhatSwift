543cdc096e19a8ed50c6b8f885e6579a1b0d7741
Allef Sousa <allefsousa_1@hotmail.com>
2020-06-25 17:56:54 -0300

    Initial Commit

---
+++ WhatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsApp
+//
+//  Created by Allef Sousa Santos on 25/06/20.
+//  Copyright © 2020 Allef Sousa Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
